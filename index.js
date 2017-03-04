4f137ed5f7c2c05bf8efe302ae2c32b8ca77bf43
Spencer Elliott <me@elliottsj.com>
2017-03-03 23:15:23 -0500

    Upgrade loader-utils to ^1.0.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var loaderUtils = require('loader-utils');
 
 module.exports = function (source) {
-  var query = loaderUtils.parseQuery(this.query);
-  var re = new RegExp(query.match.pattern, query.match.flags);
-  return source.replace(re, query.replaceWith);
+  var options = loaderUtils.getOptions(this);
+  var re = new RegExp(options.match.pattern, options.match.flags);
+  return source.replace(re, options.replaceWith);
 };
